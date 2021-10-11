f1e7ed7b56d0648751d713b883f85c535f1c8a9a
estanislao alfonso <estanislao.alf@gmail.com>
2021-10-11 10:20:00 -0300

    Creando el cartWidget y el ItemListContainer FINAL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from './components/navBar/navBar';
-import { ItemListContainer } from './components/navBar/itemListContainer';
+import { ItemListContainer } from './components/itemListContainer/itemListContainer';
 
 function App() {
   return (
